32fa00c51b2ceb1636243a87dac8e7e68b2abdff
Jhon Waree Marcelo <jhon.waree.marcelo@gmail.com>
2023-04-02 05:01:48 -0400

    Complete Using the Array Filter Method

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+function findMatching(array, name){
+
+    const drivers = array.filter(match => match.toUpperCase() === name.toUpperCase())
+    console.log(array)
+    return drivers
+}
+
+
+
+function fuzzyMatch(array, letter){
+    const drivers = array.filter(match => match.slice(0,2) === letter)
+    return drivers
+}
+
+
+function matchName(array, name){
+    
+    const drivers = array.filter(match => match.name === name)
+    
+    return drivers
+}
